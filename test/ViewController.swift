39fbfdc8f7e9f56043fcfa2a2621ead8737b3b16
Siddha Tiwari <siddhatiwari@gmail.com>
2018-10-11 19:57:52 -0400

    Inital commit

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -13,8 +13,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        button.addTarget(self, action: #selector(handleButtonTap), for: .touchUpInside)
+        
+        var width = view.widthAnchor
+        
     }
+    
+    var n = 0
 
-
+    @objc func handleButtonTap() {
+        var color = UIColor.blue
+        if n == 0 {
+            color = UIColor.red
+        } else if n == 1 {
+            color = UIColor.yellow
+        }
+        view.backgroundColor = color
+        n = n + 1
+        
+        rect.frame.size = CGSize(width: 10, height: 10)
+    }
+    
+    @IBOutlet weak var rect: UIView!
+    
+    @IBOutlet weak var button: UIButton!
 }
 
